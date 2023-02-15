a760075afe27dec6e4ad27161f76a978c5583465
Juan <salamancajd14@gmail.com>
2023-02-15 15:42:47 -0500

    Adding computed columns and selecting related objects in admin interface

--- store/admin.py
+++ store/admin.py
@@ -3,14 +3,33 @@ from . import models
 # Register your models here.
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory <10:
+            return 'LOW'
+        return 'OK'
+    list_select_related = ['collection']
+    
+    def collection_title(self, product):
+        return product.collection.title
 
 @admin.register(models.Customer)
-class CustomerAdmn(admin.ModelAdmin):
+class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership']
     list_editable = ['membership']
     list_per_page = 10
 
 admin.site.register(models.Collection)
+
+
+@admin.register(models.Order)
+class OrderAmin(admin.ModelAdmin):
+    list_display = ['placed_at', 'customer_full_name']
+    list_select_related = ['customer']
+    list_per_page=10
+    @admin.display()
+    def customer_full_name(self, order):
+        return order.customer.first_name + ' ' +order.customer.last_name
--- store/models.py
+++ store/models.py
@@ -72,6 +72,9 @@ class Order(models.Model):
         max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
+    
+    
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
