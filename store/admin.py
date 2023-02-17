256a7972347799a3bdd201198e45aa28dc551fec
Juan <salamancajd14@gmail.com>
2023-02-17 17:25:57 -0500

    Add on admin interface: Search bar, filters, custom actions, custom forms, data validators, inline children objects adding, generic relations and pluggable apps.

--- store/admin.py
+++ store/admin.py
@@ -1,17 +1,43 @@
+
 from typing import Any
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.http import HttpRequest
 from django.db.models.query import QuerySet
+
 from . import models
 from django.db.models.aggregates import Count
 from django.utils.html import format_html, urlencode
 from django.urls import reverse
 
 # Register your models here.
+
+class InventoryFilter(admin.SimpleListFilter):
+
+    title = 'inventory'
+    parameter_name = 'inventory'
+
+    def lookups(self, request: Any, model_admin: Any):
+        return [
+            ('<10', 'Low')
+        ]
+
+    def queryset(self, request: Any, queryset: QuerySet):
+        if self.value() == '<10':
+            return queryset.filter(inventory__lt = 10)
+
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+
+    autocomplete_fields = ['collection']
+    prepopulated_fields = {
+        'slug': ['title']
+    }
+    actions  = ['clear_inventory']
     list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
+    list_filter = ['collection', 'last_update', InventoryFilter]
+    search_fields = ['title']
     list_per_page = 10
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
@@ -23,12 +49,22 @@ class ProductAdmin(admin.ModelAdmin):
     def collection_title(self, product):
         return product.collection.title
 
+    @admin.action(description='Clear inventory')
+    def clear_inventory(self, request, query_set):
+        updated_count = query_set.update(inventory = 0)
+
+        self.message_user(
+            request,
+            f'{updated_count} products wew successfully updated.'
+        )
+
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership', 'total_orders']
     list_editable = ['membership']
+    
     list_per_page = 10
-
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
     @admin.display(ordering='total_orders')
     def total_orders(self, customer):
         url = (reverse('admin:store_order_changelist')
@@ -48,6 +84,7 @@ class CustomerAdmin(admin.ModelAdmin):
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ['title', 'products_count']
+    search_fields = ['title']
     @admin.display(ordering='products_count')
     def products_count(self, collection):
         url = (reverse('admin:store_product_changelist')
@@ -65,10 +102,20 @@ class CollectionAdmin(admin.ModelAdmin):
             products_count = Count('product')
         )
 
+
+class OrderItemInline(admin.StackedInline):
+    model = models.OrderItem
+    autocomplete_fields = ['product']
+    min_num = 1
+    max_num = 10
+    extra = 1
+
 @admin.register(models.Order)
 class OrderAmin(admin.ModelAdmin):
-    list_display = ['placed_at', 'customer_full_name']
+    list_display = ['id', 'placed_at', 'customer_full_name']
     list_select_related = ['customer']
+    autocomplete_fields = ['customer']
+    inlines = [OrderItemInline]
     list_per_page=10
     @admin.display()
     def customer_full_name(self, order):
