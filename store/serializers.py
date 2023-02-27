228ae2452ab781065fd832c5877a034e9c5bc638
Juan <salamancajd14@gmail.com>
2023-02-26 19:00:47 -0500

    Add: Different API functionalities like add, delete and update objects.

--- store/serializers.py
+++ store/serializers.py
@@ -2,29 +2,63 @@ from rest_framework import serializers
 from decimal import Decimal
 from store.models import Product, Collection
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length = 255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Collection
+        fields = ['id', 'title', 'products_count']
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length = 255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+    products_count = serializers.SerializerMethodField(method_name='count_products')
+    def count_products(self, collection: Collection):
+        return collection.product_set.count()
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    # This avoids the duplicated code.
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description','slug', 'inventory', 'unit_price', 'collection', 'price_with_tax']
+
+        # This is a bad practice:
+        # fields = '__all__'
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length = 255)
+    # price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
 
-    # This query the collection title
-    """ collection = serializers.PrimaryKeyRelatedField(
-        queryset = Collection.objects.all()
-    ) """
+    # # This query the collection title
+    # """ collection = serializers.PrimaryKeyRelatedField(
+    #     queryset = Collection.objects.all()
+    # ) """
 
-    #this gets the object of collections
-    #collection = CollectionSerializer()
+    # #this gets the object of collections
+    # #collection = CollectionSerializer()
 
-    #This makes an hyperlink
-    collection = serializers.HyperlinkedRelatedField(
-        queryset = Collection.objects.all(),
-        view_name= 'collection-detail'
-    )
+    # #This makes an hyperlink
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset = Collection.objects.all(),
+    #     view_name= 'collection-detail'
+    # )
 
     def calculate_tax(self, product: Product):
         return product.unit_price*Decimal(1.1)
+
+    # This makes a custom validation function
+    # def validate(self, data):
+    #     if data['password'] != data['confirm_password']:
+    #         return serializers.ValidationError('Passwords dont match')
+
+    #     return data
+
+    #This makes a custom function to save an object
+    # def create(self, validated_data):
+    #     product = Product(**validated_data)
+    #     product.other = 1
+    #     product.save()
+    #     return product
+
+
+    #This makes a custom update function 
+    # def update(self, instance, validated_data):
+    #     instance.unit_price = validated_data.get('unit_price')
+    #     instance.save()
+    #     return instance
--- store/views.py
+++ store/views.py
@@ -3,23 +3,76 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Collection
+from .serializers import ProductSerializer, CollectionSerializer
 # Create your views here.
-@api_view()
+@api_view(['GET','POST'])
 def product_list(request):
-    queryset = Product.objects.select_related('collection').all()
-    serializer = ProductSerializer(queryset, many=True, context={
-        'request':request
-    })
-    return Response(serializer.data)
-
-@api_view()
+    if request.method == 'GET':
+        queryset = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(queryset, many=True, context={
+            'request':request
+        })
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
+    
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
 
-@api_view()
+    elif request.method =='PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method =='DELETE':
+        if product.orderitem_set.count() > 0:
+            return Response({'error':'There are order items asociated with this product.'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['GET', 'POST'])
+def collection_list(request):
+    if request.method == 'GET':
+        query_set = Collection.objects.prefetch_related('product_set').all()
+        serializer = CollectionSerializer(query_set, many=True, context={
+            'request':request
+        })
+        return Response(serializer.data)
+    if request.method == 'POST':
+        serializer = CollectionSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def collection_detail(request, pk):
-    return Response('ok')
+    collection = get_object_or_404(Collection, pk=pk)
+    if request.method == 'GET':
+        serializer = CollectionSerializer(collection)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer = CollectionSerializer(collection, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method =='DELETE':
+        if collection.product_set.count() >0:
+            return Response({
+                'error': 'This collection has products associated.'
+            })
+        collection.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
